b0f7e15634471fd3afd8cb4420fc276f3cc160dc
Bodo Bogdan-Mihai <80922283+waterAddicted@users.noreply.github.com>
2021-08-29 19:12:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+'''
+Varianta 1
+100-->1
+'''
+
+n = int(input())
+r = 0
+while n:
+    r = r * 10 + n % 10
+    n = n // 10
+print(r) 
+
+
+'''
+Varianta 2 M1
+100-->001
+'''
+a = int(input())
+while a:
+    print(a%10,sep='',end='')
+    a //= 10
+print('\n')
+'''
+Varianta2 M2
+'''
+b = input()
+for i in range(len(b)-1,-1,-1):
+    print(b[i],end='')
